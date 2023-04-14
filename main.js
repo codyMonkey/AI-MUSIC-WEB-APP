7fe3e58b3c35fda8001dde0bd3fb944e3bd14626
codyMonkey <92737380+codyMonkey@users.noreply.github.com>
2023-04-14 16:21:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 song1 = "";
 song2 = "";
+left_wrist_x = 0;
+right_Wrist_x = 0;
+left_Wrist_y = 0;
+right_wrist_y = 0;
 
 function preload()
 {
     song1 = loadSound("music2.mp3");
-    song2 = loadMusic("music3.mp3");
+    song2 = loadSound("music3.mp3");
 }
 
 function setup()
@@ -16,9 +20,34 @@ function setup()
     video.hide();
 
     console.log("setup complete");
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
 }
+
+function modelLoaded()
+{
+    console.log('posenet is initialized');
+}
+
 
 function draw()
 {
     image(VIDEO, 0, 0, 600, 500);
+}
+
+function gotPoses(results)
+{
+    if(results.length > o)
+    {
+        console.log(results);
+        leftwristx = results[0].pose.leftWrist.x;
+        leftwristx = results[0].pose.leftWrist.y;
+        rightwristx = results[0].pose.righttWrist.x;
+        rightwristy = results[0].pose.rightWrist.y;
+        console.log(leftwristx, leftwristy);
+        console.log(rightwristx, rightwristy);
+        console.log("---------------------");
+
+    }
 }
